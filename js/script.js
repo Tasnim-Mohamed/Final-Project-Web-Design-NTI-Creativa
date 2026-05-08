f8ff59b73734ef3857c7cf208ef79b6c7cc1369f
Tasnim-Mohamed <tasneemkeshta30@gamil.com>
2026-05-09 02:41:52 +0300

    4th commit

--- js/script.js
+++ js/script.js
@@ -70,4 +70,7 @@ $(document).ready(function(){
 }); 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)) 
+$(document).on('click', 'a[href="#"]', function(e){
+  e.preventDefault();
+});
 
